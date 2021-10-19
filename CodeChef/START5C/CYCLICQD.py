ace662e6b3126e280d4eeac60bd2619717ee9008
Badri Vishal <bv7887393@gmail.com>
2021-10-19 20:34:29 +0530

    Added File

---
+++ CodeChef/START5C/CYCLICQD.py
@@ -0,0 +1,8 @@
+# cook your dish here
+t = int(input())
+for i in range(t):
+    a, b, c, d = map(int, input().split())
+    if (a + c) == 180 and (b + d) == 180:
+        print("YES")
+    else:
+        print("NO")
