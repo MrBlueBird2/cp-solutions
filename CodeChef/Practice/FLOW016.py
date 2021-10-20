cffbcf2ac6417b44d7b2ab784df874680729a86c
Badri Vishal <bv7887393@gmail.com>
2021-10-20 15:13:49 +0530

    Added File

--- CodeChef/Practice/FLOW016.py
+++
@@ -1,8 +0,0 @@
-# cook your dish here
-from math import gcd
-t = int(input())
-for i in range(t):
-    a, b = map(int,input().split())
-    def lcm(a,b):
-        return (a // gcd(a,b))* b
-    print(gcd(a, b), lcm(a, b))
