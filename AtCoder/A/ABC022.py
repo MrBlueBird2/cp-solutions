e16c9c4e09bdbaed0f13a5e3fe2bffbc4cdd52c6
Badri Vishal <bv7887393@gmail.com>
2021-10-20 20:08:35 +0530

    Added File

---
+++ AtCoder/A/ABC022.py
@@ -0,0 +1,12 @@
+# 22
+n,s,t=map(int,input().split())
+w=int(input())
+ans=0
+if s<=w and w<=t:
+    ans=1
+for i in range(n-1):
+    a=int(input())
+    w+=a
+    if s<=w and w<=t:
+        ans+=1
+print(ans)
