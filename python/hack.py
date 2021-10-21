c27a857f39c4310c6ed85537552f78381eb83ed5
Badri Vishal <bv7887393@gmail.com>
2021-10-21 18:28:33 +0530

    Added File

--- python/hack.py
+++ python/hack.py
@@ -8,7 +8,7 @@ for i in range(30):
     ## Add bot.txt to staging area
     os.system('git add bot.txt')
     ## Commit it
-    os.system('git commit --date="' + d + '" -m "first commit"')
+    os.system('git commit -m "first commit"')
 
 ## push the commit to github
 os.system('git push -u origin main')
