09d39ce2021b0be7cf1131afa089c0cf64951f2a
Badri Vishal <bv7887393@gmail.com>
2021-10-21 18:26:19 +0530

    Added File

--- python/hack.py
+++ python/hack.py
@@ -1,13 +1,14 @@
 import os
 
-d = '1 day ago'
-## Open a text file and modify it
-with open('bot.txt', 'a') as file:
-    file.write(d)
-## Add bot.txt to staging area
-os.system('git add bot.txt')
-## Commit it
-os.system('git commit --date="' + d + '" -m "first commit"')
+for i in range(10):
+    d = '1 day ago'
+    ## Open a text file and modify it
+    with open('bot.txt', 'a') as file:
+        file.write(d)
+    ## Add bot.txt to staging area
+    os.system('git add bot.txt')
+    ## Commit it
+    os.system('git commit --date="' + d + '" -m "first commit"')
 
 ## push the commit to github
 os.system('git push -u origin main')
