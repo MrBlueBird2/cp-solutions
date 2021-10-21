fb380f5abafca54c10297fcb6b1e33b8a531774e
Badri Vishal <bv7887393@gmail.com>
2021-10-21 18:21:36 +0530

    Added File

---
+++ AtCoder/python/hack.py
@@ -0,0 +1,13 @@
+import os
+
+d = '1 day ago'
+## Open a text file and modify it
+with open('bot.txt', 'a') as file:
+    file.write(d)
+## Add bot.txt to staging area
+os.system('git add bot.txt')
+## Commit it
+os.system('git commit --date="' + d + '" -m "first commit"')
+
+## push the commit to github
+os.system('git push -u origin master')
