668590b1772226b443901c689e4c13b87d01d4d1
Badri Vishal <bv7887393@gmail.com>
2021-10-22 18:00:58 +0530

    Added File

---
+++ AtCoder/A/ABC050.py
@@ -0,0 +1,525 @@
+# x=int(input())
+# if(x == 0):
+#     print(1)
+# else:
+#     print(0)
+
+# C=input()
+# if(C[0] == C[1] and C[1] == C[2]):
+#     print("Won")
+# else:
+#     print("Lost")
+
+# x=int(input())
+# if(x>=30):
+#     print("Yes")
+# else:
+#     print("No")
+
+# X,t=map(int,input().split())
+# if(X > t):
+#     print(X-t)
+# else:
+#     print(0)
+
+# x=int(input())
+# if(x == 7):
+#     print("YES")
+# elif(x == 5):
+#     print("YES")
+# elif(x == 3):
+#     print("YES")
+# else:
+#   print("NO")
+
+# s=input()
+# print(s.replace(","," "))
+
+# atcoder,s,contest=input().split()
+# print(atcoder[0]+s[0]+contest[0])
+
+# A,B,C=input().split()
+# if(B[0] == A[-1] and C[0] == B[-1]):
+#     print("YES")
+# else:
+#     print("NO")
+
+# X,Y=map(int,input().split())
+# print(Y//2+X)
+
+# A,B,C,D=map(int,input().split())
+# if(A+B == C+D):
+#     print("Balanced")
+# elif(A+B > C+D):
+#     print("Left")
+# else:
+#     print("Right")
+
+# contest=input()
+# print("ABC"+contest)
+
+# N=int(input())
+# A=int(input())
+# print(N*N-A)
+# x=int(input())
+# if(x == 0):
+#     print(1)
+# else:
+#     print(0)
+
+# C=input()
+# if(C[0] == C[1] and C[1] == C[2]):
+#     print("Won")
+# else:
+#     print("Lost")
+
+# x=int(input())
+# if(x>=30):
+#     print("Yes")
+# else:
+#     print("No")
+
+# X,t=map(int,input().split())
+# if(X > t):
+#     print(X-t)
+# else:
+#     print(0)
+
+# x=int(input())
+# if(x == 7):
+#     print("YES")
+# elif(x == 5):
+#     print("YES")
+# elif(x == 3):
+#     print("YES")
+# else:
+#   print("NO")
+
+# s=input()
+# print(s.replace(","," "))
+
+# atcoder,s,contest=input().split()
+# print(atcoder[0]+s[0]+contest[0])
+
+# A,B,C=input().split()
+# if(B[0] == A[-1] and C[0] == B[-1]):
+#     print("YES")
+# else:
+#     print("NO")
+
+# X,Y=map(int,input().split())
+# print(Y//2+X)
+
+# A,B,C,D=map(int,input().split())
+# if(A+B == C+D):
+#     print("Balanced")
+# elif(A+B > C+D):
+#     print("Left")
+# else:
+#     print("Right")
+
+# contest=input()
+# print("ABC"+contest)
+
+# N=int(input())
+# A=int(input())
+# print(N*N-A)
+
+# N,B,A=map(int,input().split())
+# ans=N*B
+# print(min(ans,A))
+
+# a,b,c,d=map(int,input().split())
+# ab=a*b
+# cd=c*d
+# print(max(ab,cd))
+
+# a,b=map(int,input().split())
+# add=a+b
+# sub=a-b
+# mul=a*b
+# print(max(add,sub,mul))
+
+# a,b=map(int,input().split())
+# c=b
+# d=a*b*c
+# if(d%2==1):
+#     print("Yes")
+# else:
+#     print("No")
+
+# a,b,c=map(int,input().split())
+# if(a+b >= c):
+#     print("Yes")
+# else:
+#     print("No")
+
+# contest=int(input())
+# if(contest<=999):
+#     print("ABC")
+# else:
+#     print("ABD")
+
+# r,g,b=input().split()
+# a=r+g+b
+# a=int(a)
+# if(a%4==0):
+#     print("YES")
+# else:
+#     print("NO")
+
+# n=int(input())
+# s=input()
+# red=s.count("R")
+# blue=s.count("B")
+# if(red > blue):
+#     print("Yes")
+# else:
+#     print("No")
+
+# command=input()
+# print(command.count("+") - command.count("-"))
+
+# s=input()
+# ans=""
+# if(s == "hihi" or s == "hi"):
+#     ans="Yes"
+# else:
+#     ans="No"
+# print(ans)
+# N,B,A=map(int,input().split())
+# ans=N*B
+# print(min(ans,A))
+
+# a,b,c,d=map(int,input().split())
+# ab=a*b
+# cd=c*d
+# print(max(ab,cd))
+
+# a,b=map(int,input().split())
+# add=a+b
+# sub=a-b
+# mul=a*b
+# print(max(add,sub,mul))
+
+# a,b=map(int,input().split())
+# c=b
+# d=a*b*c
+# if(d%2==1):
+#     print("Yes")
+# else:
+#     print("No")
+
+# a,b,c=map(int,input().split())
+# if(a+b >= c):
+#     print("Yes")
+# else:
+#     print("No")
+
+# contest=int(input())
+# if(contest<=999):
+#     print("ABC")
+# else:
+#     print("ABD")
+
+# r,g,b=input().split()
+# a=r+g+b
+# a=int(a)
+# if(a%4==0):
+#     print("YES")
+# else:
+#     print("NO")
+
+# n=int(input())
+# s=input()
+# red=s.count("R")
+# blue=s.count("B")
+# if(red > blue):
+#     print("Yes")
+# else:
+#     print("No")
+
+# command=input()
+# print(command.count("+") - command.count("-"))
+
+# s=input()
+# ans=""
+# if(s == "hihi" or s == "hi"):
+#     ans="Yes"
+# else:
+#     ans="No"
+# print(ans)
+
+# ac,tc=input().split()
+# if(ac == "H" and tc == "H"):
+#     print("H")
+# elif(ac == "D" and tc == "H"):
+#     print("D")
+# elif(ac == "D" and tc == "D"):
+#     print("H")
+# else:
+#     print("D")
+
+# d=int(input())
+# if(d == 25):
+#     print("Christmas")
+# elif(d == 24):
+#     print("Christmas Eve")
+# elif(d == 23):
+#     print("Christmas Eve Eve")
+# else:
+#     print("Christmas Eve Eve Eve")
+
+# n=input()
+# if("9" in n):
+#     print("Yes")
+# else:
+#     print("No")
+
+# S=input()
+# l=['a','b','c']
+# if(l[0] in S and l[1] in S and l[2] in S):
+#     print("Yes")
+# else:
+#     print("No")
+
+# a,b,c=map(int,input().split())
+# ab=a+b
+# ac=a+c
+# bc=b+c
+# print(min(ab,ac,bc))
+
+# print("2018/01/" + input()[-2:])
+
+# a=input()
+# b=input()
+# c=input()
+# print(a[0]+b[1]+c[2])
+
+# x=int(input())
+# if(x<1200):
+#     print("ABC")
+# else:
+#     print("ARC")
+
+# a,b=map(int,input().split())
+# if(a+b >= 10):
+#     print("error")
+# else:
+#     print(a+b)
+
+# n,i=map(int,input().split())
+# print(n-i+1)
+
+# yen=int(input())
+# print(yen*2)
+
+# n=input()
+# m=n[2]+n[1]+n[0]
+# ans=""
+# if(m[0] == n[0]):
+#     ans="Yes"
+#     if(m[1] == n[1]):
+#         ans="Yes"
+#         if(m[2] == n[2]):
+#             ans="Yes"
+# else:
+#     ans="No"
+# print(ans)
+
+# n=int(input())
+# if(n%2==0):
+#     print(n)
+# else:
+#     print(n*2)
+
+# a,b,c=map(int,input().split())
+# if(a != b and a != c):
+#     print(a)
+# elif(b != a and b!= c):
+#     print(b)
+# else:
+#     print(c)
+
+# x=int(input())
+# a=int(input())
+# b=int(input())
+# print(x-a%b)
+
+# H,W=map(int,input().split())
+# h,w=map(int,input().split())
+# ans = (H-h)*(W-w)
+# print(ans)
+
+# print("Welcome! To iCalculator, Have Fun! ")
+# a1=int(input("Enter First Number "))
+# a2=int(input("Enter Second Number "))
+# command=input("What Would You Like To Do? Add, Subtract and etc. ")
+# run=True
+# while run:
+#     a1=int(input("Enter First Number "))
+#     a2=int(input("Enter Second Number "))
+#     command=input("What Would You Like To Do? Add, Subtract and etc. ")
+#     if("+" in command or "add" in command):
+#         print(a1+a2)
+#     elif("-" in command or "subtract" in command):
+#         print(a1-a2)
+#     elif("*" in command or "multiply" in command):
+#         print(a1*a2)
+#     elif("/" in command or "divide" in command):
+#         print(a1//a2)
+#     else:
+#         print("TypeError: Command: '"+command+"' Not Found! ")
+
+# a,b,c=map(int,input().split())
+# ans=""
+# if(a == b and a == c):
+#     ans="Yes"
+# elif(b == a and b == c):
+#     ans="Yes"
+# elif(c == a and c == b):
+#     ans="Yes"
+# else:
+#     ans="No"
+# print(ans)
+
+# s=input()
+# print(s[1]+s[2]+s[0])
+
+# a,b=map(int,input().split())
+# c,d=map(int,input().split())
+# print(b-c)
+
+# M,H=map(int,input().split())
+# if(H%M==0):
+#     print("Yes")
+# else:
+#     print("No")
+
+# s=input()
+# print(s.count('1'))
+
+# x1,x2=map(int,input().split())
+# print(x2//x1)
+
+# N=int(input())
+# if(N%3==0):
+#     print("YES")
+# else:
+#     print("NO")
+
+# n=int(input())
+# print(n-1)
+
+# s=input()
+# print(s+"pp")
+
+# A,B=map(int,input().split())
+# print(B,A)
+
+# A=input()
+# B=input()
+# a=len(A)
+# b=len(B)
+# if(a>b):
+#     print(A)
+# else:
+#     print(B)
+
+# M,D=map(int,input().split())
+# if(M%D==0):
+#     print("YES")
+# else:
+#     print("NO")
+
+# w=input()
+# print(w+"s")
+
+# a,b=map(int,input().split())
+# if(b>a):
+#     print("Better")
+# else:
+#     print("Worse")
+
+# s=input()
+# ans = 700+100*s.count("o")
+# print(ans)
+
+# from math import gcd
+# def valid(x):
+#     return gcd(x, sum([ord(c) - ord('0') for c in str(x)])) != 1
+# t = int(input())
+# while t > 0:
+#     t -= 1
+#     n = int(input())
+#     while not valid(n):
+#         n += 1
+#     print(n)
+
+# S=input()
+# if("AC" in S):
+#     print("Yes")
+# else:
+#     print("No")
+
+# a,b=map(int,input().split())
+# add=a+b
+# mul=a*b
+# if(add == 15):
+#     print("+")
+# elif(mul == 15):
+#     print("*")
+# else:
+#     print("x")
+
+# import random
+# i=0
+# j=1
+# win = random.randint(1,100)
+# guess = 0
+# while(i != j):
+#     user = int(input("Enter A Number Between 1 To 100 "))
+#     if(user == win):
+#         print(f"You Won And You Guessed In {guess} Times ")
+#         guess+=1
+#         break
+#     elif(user < win):
+#         print("Enter A Higher Number, Please ")
+#         guess+=1
+#     else:
+#         print("Enter A Lower Number Please ")
+#         guess+=1
+
+# X = input()
+# if(X[-1] == 0):
+#     print(X[0])
+# else:
+#     x1 = X[0]
+#     x2 = X[1]
+#     x1 = int(x1)
+#     x2 = int(x2)
+#     print(x1+x2)
+
+# X=int(input())
+# if(X <= 59):
+#     print("Bad")
+# elif(X >= 60 and X <= 89):
+#     print("Good")
+# elif(X >= 90 and X <= 99):
+#     print("Great")
+# else:
+#     print("Perfect")
+
+# q=int(input())
+# if(q == 1):
+#     print("ABC")
+# else:
+#     print("chokudai")
+
+a,c,b=input().split()
+a = int(a)
+b = int(b)
+if(c != "-" and c == "+"):
+    print(a+b)
+else:
+    print(a-b)
