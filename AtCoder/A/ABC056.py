76526995d8de70fc35dc6918ace4bbab405ff455
Badri Vishal <bv7887393@gmail.com>
2021-10-23 11:41:14 +0530

    Added File

---
+++ AtCoder/A/ABC056.py
@@ -0,0 +1,9 @@
+ac,tc=input().split()
+if(ac == "H" and tc == "H"):
+    print("H")
+elif(ac == "D" and tc == "H"):
+    print("D")
+elif(ac == "D" and tc == "D"):
+    print("H")
+else:
+    print("D")
