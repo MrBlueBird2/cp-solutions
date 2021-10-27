0a71b548ba6e87ea61c52385457d025c1f351fe8
Badri Vishal <bv7887393@gmail.com>
2021-10-27 19:44:06 +0530

    Added File

---
+++ AtCoder/A/ABC196.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    /*int A, B;
+    cin >> A >> B;
+    if(A < 8 && B < 8) {
+        cout << "Yay!" << endl;
+    }else {
+        cout << ":(" << endl;
+    }
+    int a, b;
+    cin >> a >> b;
+    if(a + b == 15) {
+        cout << "+" << endl;
+    }else if(a * b == 15) {
+        cout << "*" << endl;
+    }else  {
+        cout << "x" << endl;
+    }*/
+    int a, b, c, d;
+    cin >> a >> b;
+    cin >> c >> d;
+    cout << b - c << endl;
+}
