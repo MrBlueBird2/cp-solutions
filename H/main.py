468ab0a46e50bbd1e6682be5b6ccc7b12848aa6f
Badri Vishal <bv7887393@gmail.com>
2021-10-27 19:59:32 +0530

    Added File

--- H/main.py
+++
@@ -1,9 +0,0 @@
-import os
-for i in range(1, 500):
-    os.system("sudo rm bot.txt")
-    os.system('git add -A')
-    os.system('git commit -m "Added File"')
-    os.system("touch bot.txt")
-    os.system('git add -A')
-    os.system('git commit -m "Added File"')
-os.system('git push -u origin main')
